0fcd6c5c735c3d780f2a7c1e87782e0d87b9d280
I-Nightlake <87764434+I-Nightlake@users.noreply.github.com>
2021-09-10 14:04:37 -0300

    Update laminas.py

--- laminas.py
+++ laminas.py
@@ -2,15 +2,15 @@ import os
 
 os.system("cls")
 print("---Laminas---")
-print("")
+print()
 print("Escolha uma opcao")
-print("")
+print()
 print("0 - Voltar")
-print("")
+print()
 print("1 - Motoniveladora")
-print("")
+print()
 print("2 - Perfil de nivelamento")
-print("")
+print()
 opcao_lamina = input("->")
 
 if opcao_lamina == "0":
@@ -25,6 +25,6 @@ if opcao_lamina == "2":
 if opcao_lamina not in {"0", "1", "2"}:
     os.system("cls")
     print("Comando invalido, tente novamente...")
-    print("")
+    print()
     input("Pressione enter para voltar")
     exec(open("laminas.py").read())
