d0cee624d637f31fa4782cb7b83e3127f58eb84c
I-Nightlake <87764434+I-Nightlake@users.noreply.github.com>
2021-10-05 14:58:19 -0300

    Update discos_grade_24_26.py

--- discos_grade_24_26.py
+++ discos_grade_24_26.py
@@ -15,6 +15,8 @@ def comma_to_point(expression):
 
 os.system("cls")
 
+print("---Discos Grade 24 e 26 Pol---")
+print()
 print("Desconto aplicado = -5% -15% -42,2% -##% -10,75% +5%")
 print()
 print("00 - Voltar")
