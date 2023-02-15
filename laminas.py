e1520c2d5ce069cf725b39e3a492de4b2759ad54
I-Nightlake <87764434+I-Nightlake@users.noreply.github.com>
2023-02-15 15:13:30 -0300

    Update laminas.py

--- laminas.py
+++ laminas.py
@@ -9,7 +9,9 @@ print("0 - Voltar")
 print()
 print("1 - Motoniveladora")
 print()
-print("2 - Perfil de nivelamento")
+print("2 - Motoniveladora AR-1")
+print()
+print("3 - Perfil de nivelamento")
 print()
 opcao_lamina = input("->")
 
@@ -17,14 +19,17 @@ if opcao_lamina == "0":
     exec(open("menu.py").read())
 
 if opcao_lamina == "1":
-    exec(open("laminas_moto.py").read())
+    exec(open("laminas/laminas_moto.py").read())
 
 if opcao_lamina == "2":
-    exec(open("laminas_perfil.py").read())
+    exec(open("laminas/laminas_moto_ar1.py").read())
 
-if opcao_lamina not in {"0", "1", "2"}:
+if opcao_lamina == "3":
+    exec(open("laminas/laminas_perfil.py").read())
+
+if opcao_lamina not in {"0", "1", "2", "3"}:
     os.system("cls")
     print("Comando invalido, tente novamente...")
     print()
     input("Pressione enter para voltar")
-    exec(open("laminas.py").read())
+    exec(open("laminas/laminas.py").read())
