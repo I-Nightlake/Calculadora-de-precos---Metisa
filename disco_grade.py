ad2a4a41923f0e949728526d56b39970a837d83a
I-Nightlake <87764434+I-Nightlake@users.noreply.github.com>
2023-02-15 15:09:21 -0300

    Update disco_grade.py

--- disco_grade.py
+++ disco_grade.py
@@ -24,23 +24,23 @@ if opcao_disco_grade == "00":
     exec(open("menu.py").read())
 
 if opcao_disco_grade == "0":
-    exec(open("discos.py").read())
+    exec(open("discos/discos.py").read())
 
 if opcao_disco_grade == "1":
-    exec(open("discos_grade_20_28.py").read())
+    exec(open("discos/discos_grade_20_28.py").read())
 
 if opcao_disco_grade == "2":
-    exec(open("discos_grade_22.py").read())
+    exec(open("discos/discos_grade_22.py").read())
 
 if opcao_disco_grade == "3":
-    exec(open("discos_grade_24_26.py").read())
+    exec(open("discos/discos_grade_24_26.py").read())
 
 if opcao_disco_grade == "4":
-    exec(open("discos_grade_30.py").read())
+    exec(open("discos/discos_grade_30.py").read())
 
 if opcao_disco_grade not in {"00", "0", "1", "2", "3", "4"}:
     os.system("cls")
     print("Comando invalido, tente novamente...")
     print()
     input("Pressione enter para voltar")
-    exec(open("discos_grade.py").read())
+    exec(open("discos/discos_grade.py").read())
