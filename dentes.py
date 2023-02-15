a89c2492429cce45677505fec250530ace96540f
I-Nightlake <87764434+I-Nightlake@users.noreply.github.com>
2023-02-15 15:08:44 -0300

    Update dentes.py

--- dentes.py
+++ dentes.py
@@ -53,7 +53,7 @@ while True:
         if "00000001" <= valor <= "09999999":
             raise ValueError("Numeros nao podem comecar com 0, sendo este um numero inteiro")
         if valor == "00":
-            exec(open("dentes.py").read())
+            exec(open("dentes/dentes.py").read())
     except ValueError as ve2:
         print(ve2)
     else:
@@ -92,7 +92,7 @@ while True:
     try:
         avista = input("->")
         if avista == "00":
-            exec(open("dentes.py").read())
+            exec(open("dentes/dentes.py").read())
         if avista == "1" or avista == "2":
             break
         else:
@@ -105,7 +105,7 @@ if avista == "1":
     print("Valor Unitario: R$ " + posipiroundstr)
     print()
     input("Pressione enter para voltar")
-    exec(open("dentes.py").read())
+    exec(open("dentes/dentes.py").read())
 
 if avista == "2":
     valor_avista = percent(posipiroundstr + "*3%")
@@ -116,4 +116,4 @@ if avista == "2":
     print("Valor Unitario: R$ " + posvalor_avistaroundstr)
     print()
     input("Pressione enter para voltar")
-    exec(open("dentes.py").read())
+    exec(open("dentes/dentes.py").read())
